785ecc6be83a87b0de3920ed32da3ee34dba1be8
Mouadh Mkadmi <36245246+wvagen@users.noreply.github.com>
2020-11-24 17:12:00 +0100

    Version 6.0
    SlideAnswer Game implemented

--- Assets/MyScripts/Quiz_SlideAnswer/SldieAnswer_MasterClass.cs
+++ Assets/MyScripts/Quiz_SlideAnswer/SldieAnswer_MasterClass.cs
@@ -8,20 +8,73 @@ public class SldieAnswer_MasterClass : MonoBehaviour
 
     public Quiz_Game_Over_Manager gameOverManager;
 
+    public Transform dropAreaTransform;
+
     public AudioSource myAudioSource;
     public AudioClip successAudioClip, FailureAudioClip;
 
     public Button confirmBtn;
 
-    // Start is called before the first frame update
+    SlideAnswer_Game_Suggestion sliderGameSugg;
+
+    bool isFilled =false;
+
+    const float MIN_DISTANCE_TO_SNAP = 2.5f;
     void Start()
     {
-        
+        ConfirmBtnBehaviour(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void Confirm_Btn()
     {
-        
+        if (this.sliderGameSugg.isRightAnswer)
+        {
+            myAudioSource.PlayOneShot(successAudioClip);
+            gameOverManager.WinLoseLevelManager(true, 3);
+        }
+        else
+        {
+            gameOverManager.WinLoseLevelManager(false);
+            myAudioSource.PlayOneShot(FailureAudioClip);
+        }
+    }
+
+    public void Snap_Drop_Area_Behaviour(SlideAnswer_Game_Suggestion sliderGameSugg)
+    {
+        if (!isFilled && Vector2.Distance(sliderGameSugg.transform.position, dropAreaTransform.position) <= MIN_DISTANCE_TO_SNAP)
+        {
+            sliderGameSugg.Snap_Me(dropAreaTransform.position, true);
+            this.sliderGameSugg = sliderGameSugg;
+            isFilled = true;
+            ConfirmBtnBehaviour(true);
+        }
+        else
+        {
+            sliderGameSugg.Snap_Me(sliderGameSugg.initPos, false);
+            if (this.sliderGameSugg.Equals(sliderGameSugg) && isFilled)
+            {
+                isFilled = false;
+                ConfirmBtnBehaviour(false);
+            }
+        }
+    }
+
+    void ConfirmBtnBehaviour(bool isConfirmEnabled)
+    {
+        Color confirmBtnCol = confirmBtn.GetComponent<Image>().color;
+        if (isConfirmEnabled)
+        {
+            confirmBtn.enabled = true;
+            confirmBtnCol.a = 1;
+        }
+        else
+        {
+            confirmBtn.enabled = false;
+            confirmBtnCol.a = 0.5f;
+        }
+
+        confirmBtn.GetComponent<Image>().color = confirmBtnCol;
     }
 }
--- Assets/MyScripts/Quiz_SlideAnswer/SlideAnswer_Game_Suggestion.cs
+++ Assets/MyScripts/Quiz_SlideAnswer/SlideAnswer_Game_Suggestion.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class SlideAnswer_Game_Suggestion : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SldieAnswer_MasterClass slideAnswerMasterClass;
+
+    public AudioSource myAudioSource;
+    public AudioClip myAnswerClip;
+
+    public Vector2 initPos;
+
+    public bool isRightAnswer = false;
+
+    bool isHolding = false,isSnapped = false;
+
+
     void Start()
     {
-        
+        initPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isHolding && !isSnapped) HoldingBehaviour();
     }
+
+    void HoldingBehaviour()
+    {
+      Vector2 fingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      transform.position = fingerPos;
+    }
+
+    public void OnPointerDown()
+    {
+        isHolding = true;
+        isSnapped = false;
+    }
+
+    public void OnPointerUp()
+    {
+        isHolding = false;
+        slideAnswerMasterClass.Snap_Drop_Area_Behaviour(this);
+    }
+
+    public void Snap_Me(Vector3 snapPos, bool isSnapped)
+    {
+        this.isSnapped = isSnapped;
+        transform.position = snapPos;
+    }
+
+    public void Listen_To_Answer_Btn()
+    {
+        myAudioSource.PlayOneShot(myAnswerClip);
+    }
+
 }
