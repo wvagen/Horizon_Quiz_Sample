1f8e7fa799583eb2aefe696c14e26594619fd3be
Mouadh Mkadmi <wvagen@gmail.com>
2021-05-04 14:32:10 +0100

    Version 15.0

---
+++ Assets/MyScripts/Quiz_Like_Dislike/Like_Dislike_MasterClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Like_Dislike_MasterClass : MonoBehaviour
+{
+    public Quiz_Game_Over_Manager quizGOMan;
+
+    public void Select_A_Choice(Like_Dislike_Suggestion selectedAnswer)
+    {
+        StartCoroutine(Win_Level());
+    }
+
+    IEnumerator Win_Level()
+    {
+        yield return new WaitForSeconds(2);
+        quizGOMan.WinLoseLevelManager(true,3);
+    }
+
+}
---
+++ Assets/MyScripts/Quiz_Like_Dislike/Like_Dislike_Suggestion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Like_Dislike_Suggestion : MonoBehaviour
+{
+    public Like_Dislike_MasterClass masterClass;
+
+    bool isSelected = false;
+    public void Select_Me()
+    {
+        if (!isSelected)
+        {
+            isSelected = true;
+            masterClass.Select_A_Choice(this);
+            StartCoroutine(Rotate_Animation_Coroutine());
+        }
+    }
+
+    IEnumerator Rotate_Animation_Coroutine()
+    {
+        while (true)
+        {
+            transform.Rotate(Vector2.up * Time.deltaTime * 400);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
